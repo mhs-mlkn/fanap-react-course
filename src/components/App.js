e8bcb785e862e5ff9dde7c77b4aa08ce8a4689de
Your Name <mhs.malekan@gmail.com>
2020-10-18 17:52:06 +0330

    comit

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import PostList from "./PostList";
-import Post from "./Post";
-import usePosts from "./usePosts.hook";
-
-const useStyles = makeStyles(theme => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
-  }
-}));
 
 export default function App() {
-  const classes = useStyles();
-  const [post, setPost] = useState();
-  const { loading, posts } = usePosts();
-
-  const handleSelectPost = p => setPost(p);
-
-  const handleBackToList = p => setPost(undefined);
-
   return (
     <React.Fragment>
       <CssBaseline />
@@ -38,18 +17,8 @@ export default function App() {
         </Toolbar>
       </AppBar>
       <main>
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
-            {loading ? (
-              <div style={{ width: "100%", textAlign: "center" }}>
-                <CircularProgress />
-              </div>
-            ) : post ? (
-              <Post post={post} onBack={handleBackToList} />
-            ) : (
-              <PostList posts={posts} onSelectPost={handleSelectPost} />
-            )}
-          </Grid>
+        <Container maxWidth="md">
+          <h1>App</h1>
         </Container>
       </main>
     </React.Fragment>
---
+++ src/components/withData.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function withData(api) {
+  return function name(WrappedComponent) {
+    return class 
+  }
+}
